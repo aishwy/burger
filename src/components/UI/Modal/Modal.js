3dfc8ac6651f69d6e4d933a791a5ffffb22bc57d
aishwarya <aishwarya.singhal@indiamart.com>
2018-11-28 10:21:35 +0530

    update

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import classes from './Modal.css';
 class Modal extends Component{
     shouldComponentUpdate(nextProps,nextState) {
-        return nextProps.show != this.props.show;
+        return nextProps.show != this.props.show || nextProps.children != this.props.children ;
     }
     render(){
         return (
